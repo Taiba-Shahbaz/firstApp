27748b92326dff6ec474aa71bde4842e19b594e9
Taiba-Shahbaz <201370153@gift.edu.pk>
2023-08-25 23:28:51 +0500

    initials

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Taiba shahbaz</h1>
+        <h2>Roll num :201370153</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
